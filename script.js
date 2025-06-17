f274b7a619ef5900247c8c82011f42b2207fa730
we0ut <andrej.vuckov456@gmail.com>
2025-06-18 01:14:16 +0200

    Some changes to home
    Added education page and made the year show dynamically

---
+++ script.js
@@ -0,0 +1,5 @@
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
